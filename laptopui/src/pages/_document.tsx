462f66037ff93342911c86c7a62366d459a2fcef
shashishsoni <shshshsoni2003@tutanota.com>
2025-02-14 20:14:08 +0530

    fixed small issue

--- laptopui/src/pages/_document.tsx
+++ laptopui/src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/image.png" />
+        <meta name="description" content="Premium Gaming Laptops - LaptopUI" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
